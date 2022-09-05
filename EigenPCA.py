9b35c890366962cd6050d8aa3f7c66658e711c52
Nicholas T. Smith <nicholastoddsmith@gmail.com>
2022-09-04 21:02:14 -0700

    Add EigenPCA class

---
+++ EigenPCA.py
@@ -0,0 +1,99 @@
+import numpy        as     np
+from   scipy.sparse import spmatrix
+
+def SparseCov(A):
+   m  = A.shape[0]
+   As = A.sum(0, keepdims=True) if isinstance(A, np.ndarray) else A.sum(0)
+   return A.T @ A - As.T @ (As * (1 / m))
+
+def SparseCovT(A):
+   Am  = A.mean(0, keepdims=True) if isinstance(A, np.ndarray) else A.mean(0)
+   AAm = A @ Am.T
+   Am2 = Am @ Am.T
+   return A @ A.T - AAm - AAm.T + Am2
+
+class EigenPCA:
+   
+   def __init__(self, n_components=-1, scale=True, T=None):
+      self.scale = scale
+      self.nc    = n_components
+      self.T     = T
+   
+   def fit(self, X, sample_weight=None):
+      m, n = X.shape
+      W = ((np.full((m, 1), 1 / m))
+              if sample_weight is None else
+           (sample_weight / sample_weight.sum()))
+      if len(W.shape) == 1:
+         W = W[:, None]
+
+      if self.nc < 0:
+         self.nc_ = min(m, n)
+      else:
+         self.nc_ = min(self.nc, m, n)
+      
+      self.mean_ = Xm = W.T @ X
+      
+      if self.scale:
+         if isinstance(X, spmatrix):
+            Xd = np.sqrt(W.T @ X.power(2) - np.square(W.T @ X))
+            X  = X.multiply(1 / Xd)
+         else:
+            Xd = np.sqrt(W.T @ np.square(X) - np.square(W.T @ X))
+            X = X * (1 / Xd)
+         self.std_ = Xd
+      
+      Wr = np.sqrt(W)
+      if (not self.T) and (m > n):   # Use (X^T)X
+         if isinstance(X, spmatrix):
+            Xw = X.multiply(Wr)
+         else:
+            Xw = X * Wr
+
+         C = Xw.T @ Xw - Xm.T @ Xm
+      else:                          # Use X(X^T)
+         XXm = X @ Xm.T
+         C   = X @ X.T - XXm - XXm.T + Xm @ Xm.T
+         C   = Wr * C * Wr.T
+      
+      S, Q = np.linalg.eigh(C)
+      S = S[::-1]                    # eigh sorts in ascending order
+      Q = Q[:, ::-1]
+      
+      self.exp_var_ = S
+      self.tot_var_ = np.sum(self.exp_var_ )
+      self.exp_rat_ = self.exp_var_  / self.tot_var_
+   
+      # nc_ can also be x in [0, 1] and is taken to mean: take the largest
+      # component until the total explained variance is >= x
+      if isinstance(self.nc_, float):
+         c_ratio = np.cumsum(self.exp_rat_)
+         self.nc_ = \
+            np.searchsorted(c_ratio, self.nc_, side='right') + 1
+         
+      Qnc = Q[:, :self.nc_]
+      
+      if (not self.T) and (m > n):
+         C = Qnc     # eigh returns normalized eigenvectors
+      else:
+         # (A-u)@(A-u)^T = Z and  Z = (P @ D @ P^T) then
+         # (A-u)^T @ P is eigenvector matrix for (A-u)^T @ (A-u)
+         # Wr * (X - u).T @ Q == (Wr * X).T @ Q - np.outer(u, Wr.T @ Q)
+         C  = (Wr * X).T @ Qnc - Xm.T * (Wr.T @ Qnc)
+         C /= np.linalg.norm(C, ord=2, axis=0, keepdims=True)  # Need to re-normalize
+      
+      self.components_ = C
+      
+      return self
+
+   def fit_transform(self, X, sample_weight=None):
+      return self.fit(X, sample_weight=sample_weight).transform(X)
+
+   def transform(self, X):
+      if self.scale:
+         if isinstance(X, spmatrix):
+            X = X.multiply(1 / self.std_)
+         else:
+            X = X * (1 / self.std_)
+            
+      return X @ self.components_ - self.mean_ @ self.components_
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# WeightedPCA
-Simple Python class for performing a Weighted PCA (WPCA).
+# Weighted PCA
+Python classes for performing a weighted principal component analysis (PCA).
+
+## WPCA
+A class for performing a weighted principal component analysis (WPCA) using the singular value decomposition (SVD).
 
 See [this blog plost](https://nicholastsmith.wordpress.com/2022/07/08/weighted-pca/) for more details on how the algorithm functions.
 
-## Example
+### Example
 ```python
 
 import numpy as np
@@ -16,4 +19,20 @@ W = np.random.randint(1, 10, size=m)
 
 wpca = WPCA().fit(A, W)
 TA   = np.repeat(wpca.transform(A), W, axis=0)
+```
+
+## EigenPCA
+A class for performing a weighted principal component analysis (WPCA) using the eigenvalue decomposition of either `XX*` or `X*X`. If not specified, the approach is chosen to the be the more efficient of the two. The method supports sparse matrices and is able to efficiently decompose <i>m</i>x<i>n</i> rectangular matrices (i.e. when `m << n` or `m >> n`)
+
+### Example
+```python
+
+from scipy.sparse import random
+from EigenPCA import EigenPCA
+
+Z = random(100000, 1000, density=0.005, format='csc')
+
+EP = EigenPCA(n_components=4)
+
+X = EP.fit_transform(SM).A
 ```
