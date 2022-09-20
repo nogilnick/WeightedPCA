38bb5afdd838ebf3469431d82ff286cc41ae4d97
Nicholas T. Smith <nicholastoddsmith@gmail.com>
2022-09-19 21:57:24 -0700

    Handle np.matrix

--- EigenPCA.py
+++ EigenPCA.py
@@ -3,6 +3,12 @@ from   scipy.sparse import spmatrix
 
 class EigenPCA:
    
+   def __str__(self):
+      return '{}({})'.format(self.__class__.__name__, getattr(self, 'nc_', ''))
+   
+   def __repr__(self):
+      return self.__str__()   
+
    def __init__(self, n_components=-1, scale=True, T=None):
       self.scale = scale
       self.nc    = n_components
@@ -33,17 +39,16 @@ class EigenPCA:
          self.std_ = Xd
       
       Wr = np.sqrt(W)
+      if isinstance(X, spmatrix):
+         Xw = X.multiply(Wr)
+      else:
+         Xw = X * Wr
       if (not self.T) and (m > n):   # Use (X^T)X
-         if isinstance(X, spmatrix):
-            Xw = X.multiply(Wr)
-         else:
-            Xw = X * Wr
-
          C = Xw.T @ Xw - Xm.T @ Xm
       else:                          # Use X(X^T)
          XXm = X @ Xm.T
-         C   = X @ X.T + (Xm @ Xm.T - XXm) - XXm.T 
-         C   = Wr * C * Wr.T
+         C   = X @ X.T + (Xm @ Xm.T - XXm) - XXm.T
+         C   = np.multiply(np.multiply(Wr, C), Wr.T)
       
       S, Q = np.linalg.eigh(C)
       S = S[::-1]                    # eigh sorts in ascending order
@@ -68,7 +73,7 @@ class EigenPCA:
          # (A-u)@(A-u)^T = Z and  Z = (P @ D @ P^T) then
          # (A-u)^T @ P is eigenvector matrix for (A-u)^T @ (A-u)
          # Wr * (X - u).T @ Q == (Wr * X).T @ Q - np.outer(u, Wr.T @ Q)
-         C  = (Wr * X).T @ Qnc - Xm.T * (Wr.T @ Qnc)
+         C  = Xw.T @ Qnc - np.multiply(Xm.T, Wr.T @ Qnc)
          C /= np.linalg.norm(C, ord=2, axis=0, keepdims=True)  # Need to re-normalize
       
       self.components_ = C
